71d52d1f7dda16a1ded49769c87ad9a5d6163e1a
epstarr <epstarr17@gmail.com>
2017-02-18 09:17:53 -0600

    add hero array and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { Hero } from './hero';
 import { AppComponent }  from './app.component';
 
 @NgModule({
