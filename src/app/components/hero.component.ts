6a3f46226785a42d26ad8243354462c27f7c62b0
epstarr <epstarr17@gmail.com>
2017-02-17 22:29:24 -0600

    select hero from options and display in header

--- src/app/components/hero.component.ts
+++ src/app/components/hero.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hero',
   template: `<h1>Hero: {{hero}}</h1>
-  <select name="heroSelect" ng>
+  <select name="heroSelect" [(ngModel)]="hero">
   	<option *ngFor="let hero of heroes">{{hero}}</option>
   </select>`,
 })
 export class HeroComponent  { 
-	heroName: string; 
+	hero: string; 
 	heroes = ["Nino", "Abel", "Chrom"];
 
 constructor() {
-		this.heroName = 'Choose a Hero';
+		this.hero = 'Choose a Hero';
 	}
 }
