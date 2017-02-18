cf89f13584009a3705a58f855afc11b6136312cc
epstarr <epstarr17@gmail.com>
2017-02-18 17:09:33 -0600

    big update - able to select array data through select option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+export class Hero {
+	name: string;
+	hp: number;
+}
 
 @Component({
   selector: 'my-app',
-  template: `<hero></hero>`,
+  template: `<h1>Choose Your Heroes</h1>
+  <div *ngIf="selectedHero">
+  <h2>Hero: {{selectedHero.name}}</h2>
+  <p>HP: {{selectedHero.hp}}</p>
+  </div>
+  <select name="heroSelect" [(ngModel)]="selectedHero">
+  	<option *ngFor="let h of heroes" [ngValue]="h">{{h.name}}</option>
+  </select>`,
 })
 export class AppComponent  { 
+	hero: string; 
+	heroes = HEROES;
+	selectedHero: Hero;
 
+constructor() {
+
+	}
 }
+const HEROES: Hero[] = [
+	{name: 'Chrom', hp: 34},
+	{name: 'Abel', hp: 44},
+	{name: 'Lucina', hp: 12}
+];
+
+
--- src/app/components/hero.component.ts
+++ src/app/components/hero.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
+export class Hero {
+	id: number;
+	name: string;
+}
 
 @Component({
   selector: 'hero',
-  template: `<h1>Hero: {{hero}}</h1>
+  template: `<h1>Choose Your Heroes</h1>
+
   <select name="heroSelect" [(ngModel)]="hero">
-  	<option *ngFor="let hero of heroes">{{hero}}</option>
+  	<option *ngFor="let hero of heroes">{{hero.name}}</option>
   </select>`,
 })
 export class HeroComponent  { 
 	hero: string; 
-	heroes = ["Nino", "Abel", "Chrom"];
+	heroes = HEROES;
+
 
 constructor() {
 		this.hero = 'Choose a Hero';
 	}
 }
+const HEROES: Hero[] = [
+	{id: 1, name: 'Chrom'},
+	{id: 2, name: 'Abel'},
+	{id: 3, name: 'Lucina'}
+];
+
+
