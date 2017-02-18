c599ae09dab749eabe71836288a3d19da3a7b377
epstarr <epstarr17@gmail.com>
2017-02-18 17:30:41 -0600

    add hero icon support and more stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,31 @@ import { Component } from '@angular/core';
 export class Hero {
 	name: string;
 	hp: number;
+	atk: number;
+	spd: number;
+	def: number;
+	res: number;
+	weaponMight: number;
+}
+
+interface weapon {
+	might: number;
 }
 
 @Component({
   selector: 'my-app',
   template: `<h1>Choose Your Heroes</h1>
   <div *ngIf="selectedHero">
+  <img src="../images/Icon_Portrait_{{selectedHero.name}}.png" />
   <h2>Hero: {{selectedHero.name}}</h2>
-  <p>HP: {{selectedHero.hp}}</p>
+  <p>HP: {{selectedHero.hp}}<br/>
+  Atk: {{selectedHero.atk + selectedHero.weaponMight}}</p>
   </div>
   <select name="heroSelect" [(ngModel)]="selectedHero">
   	<option *ngFor="let h of heroes" [ngValue]="h">{{h.name}}</option>
-  </select>`,
+  </select>
+  <h3>Full Data</h3>
+  <p>{{selectedHero | json}}</p>`,
 })
 export class AppComponent  { 
 	hero: string; 
@@ -25,9 +38,9 @@ constructor() {
 	}
 }
 const HEROES: Hero[] = [
-	{name: 'Chrom', hp: 34},
-	{name: 'Abel', hp: 44},
-	{name: 'Lucina', hp: 12}
+	{name: 'Abel', hp: 44, atk: 41, spd: 27, def: 25, res:25, weaponMight: 33},
+	{name: 'Abel', hp: 44, atk: 41, spd: 27, def: 25, res:25, weaponMight: 33},
+	{name: 'Lucina', hp: 44, atk: 41, spd: 27, def: 25, res:25, weaponMight: 33}
 ];
 
 
