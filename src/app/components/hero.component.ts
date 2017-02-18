0a5c34e5c92e8b5eca9e70bccb4271d42a2c7590
epstarr <epstarr17@gmail.com>
2017-02-17 22:01:08 -0600

    Generate hero select options from array

--- src/app/components/hero.component.ts
+++ src/app/components/hero.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hero',
-  template: `<h1>Hero: {{heroName}}</h1>
-  <select name="heroSelect" [(ngModel)]="heroName">
-  	<option value="Abel">Abel</option>
-  	<option value="Nikki">Nikki</option>
+  template: `<h1>Hero: {{hero}}</h1>
+  <select name="heroSelect" ng>
+  	<option *ngFor="let hero of heroes">{{hero}}</option>
   </select>`,
 })
 export class HeroComponent  { 
 	heroName: string; 
-
+	heroes = ["Nino", "Abel", "Chrom"];
 
 constructor() {
 		this.heroName = 'Choose a Hero';
