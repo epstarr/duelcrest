e79dd614a0334931165c57856de0539ded36dcc3
epstarr <epstarr17@gmail.com>
2017-02-18 18:24:58 -0600

    import Hero class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>Choose Your Heroes</h1>
-  <hero></hero>`,
+  <hero></hero>
+  <h3>Full selectedHero JSON Data</h3>
+  <p>{{selectedHero | json}}</p>`,
 })
 export class AppComponent  { 
-
+	hero: Hero;
 
 constructor() {
 
