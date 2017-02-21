8bde4ccf4af92d69534e9b07b4c7b4d75954e255
epstarr <epstarr17@gmail.com>
2017-02-20 20:10:51 -0600

    add a few more heroes

--- src/app/heroes.ts
+++ src/app/heroes.ts
@@ -7,5 +7,9 @@ import { Hero } from './hero';
 //Allow for export to other areas of the app
 export const HEROES: Hero[] = [
 	{ name: 'Abel', hp: 44, atk: 41, spd: 27, def: 25, res:25, weaponMight: 8 },
-	{ name: 'Lucina', hp: 44, atk: 41, spd: 27, def: 25, res:25, weaponMight: 33 }
+	{ name: 'Alfonse', hp: 43, atk: 51, spd: 25, def: 32, res:22, weaponMight: 16 },
+	{ name: 'Anna', hp: 41, atk: 45, spd: 38, def: 22, res:28, weaponMight: 16 },
+	{ name: 'Nino', hp: 33, atk: 46, spd: 36, def: 19, res:29, weaponMight: 13 },
+	{ name: 'Serra', hp: 33, atk: 36, spd: 40, def: 18, res:33, weaponMight: 4 }
+
 ];
